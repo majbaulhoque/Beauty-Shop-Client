80935c52a68eb2a7be74531567ff0265278607e8
Majba UL Hoque <majba9399@gmail.com>
2024-09-09 20:50:19 +0600

    added uploaded image part

--- src/Component/Home/OurProducts.jsx
+++ src/Component/Home/OurProducts.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import AddIcon from '../../assets/images/add.png';
 
 const OurProducts = () => {
     const [productData, setProductData] = useState([]);
-    const [isError, setIsError] = useState(false);
+    const [isError, setIsError] = useState('');
+    const [images, setImages] = useState([]);
+    const inputFileRef = useRef(null);
 
     const getProductData = async () => {
         try {
@@ -14,26 +17,50 @@ const OurProducts = () => {
         }
     };
 
+    const handleImageUploadFile = () => {
+        inputFileRef.current.click();
+    };
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImages(prevImages => [...prevImages, file]);
+        }
+    };
+
     useEffect(() => {
         getProductData();
     }, []);
 
     return (
         <div>
-            <div className="max-w-7xl mx-auto">
-                <h3 className="text-black font-bold text-center text-4xl my-8">Our Products</h3>
-                {isError !== '' && <h2 className="text-red-600 text-center text-3xl font-bold">{isError}</h2>}
+            <div className="max-w-7xl mx-auto mt-28">
+                <h3 className="text-black font-bold text-center text-4xl mb-20">Our Products</h3>
+                {isError && <h2 className="text-red-600 text-center text-3xl font-bold">{isError}</h2>}
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-5 max-w-6xl mx-auto">
                     {productData?.map((product) => {
                         const { id, img } = product || {};
                         return (
-                            <div key={id} className="card w-full h-full shadow">
+                            <div key={id} className="card h-full shadow">
                                 <div className="w-full h-64 overflow-hidden rounded-lg">
                                     <img src={img} alt="Our Product" className="w-full h-full object-cover" />
                                 </div>
                             </div>
                         );
                     })}
+                    {images.map((image, index) => (
+                        <div key={index} className="card h-full shadow">
+                            <div className="w-full h-64 overflow-hidden rounded-lg">
+                                <img src={URL.createObjectURL(image)} alt={`Uploaded ${index}`} className="w-full h-full object-cover" />
+                            </div>
+                        </div>
+                    ))}
+                    {(images.length === 0 || images.length < 4) && (
+                        <div className="card flex justify-center items-center h-full rounded shadow" onClick={handleImageUploadFile}>
+                            <img src={AddIcon} className="h-64 w-64" alt="Add Icon" />
+                            <input type="file" ref={inputFileRef} onChange={handleImageChange} style={{ display: 'none' }} />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
